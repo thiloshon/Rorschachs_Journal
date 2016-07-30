7bd9212687835c073716329a7934b7f409f3b802
darkDarthVadar <yohan.2015282@iit.ac.lk>
2016-07-30 19:25:44 +0530

    Add files via upload

--- Yohan/connectDB.php
+++ Yohan/connectDB.php
@@ -1,7 +1,7 @@
 <?php
 //set up the database security
     $host = "localhost";
-    $dbuser = "root";
+    $dbuser = "customer";
     $dbpass = "";
     $dbase = "rorschachs journal";
 
@@ -17,6 +17,22 @@
         die("Connection to the database failed");
     }
 
+//getting all the form details
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $rating = $_POST['ratings'];
+    $comment = $_POST['comments'];
 
+//sql query to insert data into the database
+    $select = "insert into commentsdb(name, email, rating, comment) values ('".$name."','".$email."','".$rating."','".$comment."')";
+    if ($sql = mysqli_query($conn,$select)){
+        mysqli_close($conn);
+        $msg = "Successfully sent!";
+    }else{
+        $msg = "Sending failed!";
+    }
+
+//displaying appropiate messages whether data is stored properly
+    echo '<h1 align="center">'.$msg.'</h1>';
 
 ?>
