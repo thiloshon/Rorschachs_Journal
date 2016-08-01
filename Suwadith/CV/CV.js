ea5b4ba708f9295d42917e37f5079cc47680f7d0
Suwadith <suwadith06@gmail.com>
2016-08-02 00:12:32 +0530

    Added onClick Functions!

--- Suwadith/CV/CV.js
+++ Suwadith/CV/CV.js
@@ -1,3 +1,23 @@
 /**
  * Created by Suwadith on 7/31/2016.
  */
+
+function clickThiloshon(){
+    document.getElementById("ourTeam").setAttribute('class', 'hidden');
+    document.getElementById("thiloshon").setAttribute('class', 'display');
+}
+
+function clickMalik(){
+    document.getElementById("ourTeam").setAttribute('class', 'hidden');
+    document.getElementById("malik").setAttribute('class', 'display');
+}
+
+function clickSuwadith(){
+    document.getElementById("ourTeam").setAttribute('class', 'hidden');
+    document.getElementById("suwadith").setAttribute('class', 'display');
+}
+
+function clickYohan(){
+    document.getElementById("ourTeam").setAttribute('class', 'hidden');
+    document.getElementById("yohan").setAttribute('class', 'display');
+}
