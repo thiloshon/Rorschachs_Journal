002212c839ac644e6672377e8004a30a49f81130
MalikPerera <malik.2015124@iit.ac.lk>
2016-08-04 16:44:08 +0530

    test

--- Malik/Poll/pollcookie.js
+++ Malik/Poll/pollcookie.js
@@ -1,5 +1,6 @@
 <script>
 $(document).ready(function(){
+
     $("#poll").click(function(){
         var count = '';
         if (document.getElementById("vote1").checked) {
@@ -8,33 +9,27 @@ $(document).ready(function(){
         if (document.getElementById("vote2").checked) {
             count = 1;
         }
-
         var jsonV= { "vote": count };
 
-        if ( sessionStorage.getItem( 'voted' ) == 'true' ) {
-            alert('You have already voted!');
-        }else if ( sessionStorage.getItem( 'voted' ) == null ){
-            $.ajax({
-                type  : "POST",
-                url   : "poll_vote.php",
-                data  : jsonV,
-                dataType: "json",
-                success : function ( responseText ){
-                    console.log("It's working" + responseText);
-                    $("#result").html( responseText.vote );
-                },
-                complete : function(){
-                    $("#poll").slideUp();
-                    sessionStorage.setItem('voted', 'true');
-                },
-                error : function( error,responseText ){
-                    // alert("Server not Responding. Sorry for the inconvenience caused. Please Try again Later");
-                    console.log( error );
-                    $("#result").html( error + responseText );
-                    alert( count );
-                }
-            });
-        }
+        $.ajax({
+            type  : "POST",
+            url   : "poll_vote.php",
+            data  : jsonV,
+            dataType: "json",
+            success : function ( responseText ){
+                console.log("Is working " + responseText);
+                $("#result").html( responseText.vote );
+            },
+            complete : function(){
+                $("#poll").slideUp();
+            },
+            error : function( error,responseText ){
+                // alert("Server not Responding. Sorry for the inconvenience caused. Please Try again Later");
+                console.log( error );
+                $("#result").html( error + responseText );
+                alert( count );
+            }
+        });
     });
 });
 </script>
