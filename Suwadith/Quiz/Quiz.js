6893f9ef7e27c9d77b344ffc493d711a33c0c276
Suwadith <suwadith06@gmail.com>
2016-08-04 15:35:08 +0530

    Added Comments. Waiting for the CSS from Thilo

--- Suwadith/Quiz/Quiz.js
+++ Suwadith/Quiz/Quiz.js
@@ -4,17 +4,17 @@
 
 function start() {
 
-    document.getElementById("startMenu").setAttribute('class', 'hide');
-    document.getElementById("radioButtons").setAttribute('class', 'show');
-    document.getElementById("timer").setAttribute('class', 'show');
+    document.getElementById("startMenu").setAttribute('class', 'hide'); /*hiding the startMenu div*/
+    document.getElementById("radioButtons").setAttribute('class', 'show'); /*displaying the questions*/
+    document.getElementById("timer").setAttribute('class', 'show'); /*displaying the timer*/
     timer();
 }
 
 function showMarks() {
 
-    document.getElementById("radioButtons").setAttribute('class', 'hide');
-    document.getElementById("timer").setAttribute('class', 'hide');
-    document.getElementById("resultsMenu").setAttribute('class', 'show');
+    document.getElementById("radioButtons").setAttribute('class', 'hide'); /*hiding the questions*/
+    document.getElementById("timer").setAttribute('class', 'hide'); /*hiding the timer*/
+    document.getElementById("resultsMenu").setAttribute('class', 'show'); /*displaying the results*/
     checkAnswers();
     seconds = 0;
 }
@@ -24,10 +24,10 @@ var seconds = 0;
 function timer() {
     seconds = 100;
     var interval = setInterval(function () {
-        document.getElementById('timer').innerHTML = "Seconds Left " + --seconds;
+        document.getElementById('timer').innerHTML = "Seconds Left " + --seconds; /*displaying the remaining time*/
 
         if (seconds == 0) {
-            document.getElementById('timer').innerHTML = 'Time up';
+            document.getElementById('timer').innerHTML = 'Time up'; /*Displying the time up  message*/
             clearInterval(interval);
             showMarks();
         }
@@ -36,31 +36,32 @@ function timer() {
 
 
 var marks = 0;
-var correctAnswers = ["b", "b", "b", "a", "b", "a", "b", "c", "a", "d"];
-var correctAnswer = "you've chosen the correct answer. It's  ";
-var incorrectAnswer = "Incorrect answer. Correct Answer is ";
-var didntChoose = "You didn't choose an answer. Correct Answer is ";
-var correctChoice = ["Casablanca", "Sherlock Holmes", "A Beautiful Mind", "Queen of Hearts", "The Curious Case of Benjamin Button", "Chicken", "Chicago", "Child's Play", "Up", "The Return of the King"];
+var correctAnswers = ["b", "b", "b", "a", "b", "a", "b", "c", "a", "d"]; /*Array which holds the button IDs of the correct answers*/
+var correctAnswer = "you've chosen the correct answer. It's  "; /*Prefix of the message to be displayed when the correct answer was chosen*/
+var incorrectAnswer = "Incorrect answer. Correct Answer is "; /*Prefix of the message to be displayed when an incorrect answer was chosen*/
+var didntChoose = "You didn't choose an answer. Correct Answer is "; /*Prefix of the message to be displayed when the user didn't choose an answer*/
+var correctChoice = ["Casablanca", "Sherlock Holmes", "A Beautiful Mind", "Queen of Hearts", "The Curious Case of Benjamin Button",
+    "Chicken", "Chicago", "Child's Play", "Up", "The Return of the King"]; /*Array which holds the correct set of answers*/
 var radios;
 
 
 function checkAnswers() {
     radios = document.getElementsByName("q1");
-    document.getElementById("displayQuestion1").innerHTML = document.getElementById("question1").innerHTML;
+    document.getElementById("displayQuestion1").innerHTML = document.getElementById("question1").innerHTML; /*Displays the 1st question*/
     for (var i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
+        if (radios[i].checked) { /*Identifies the chosen answer*/
             (radios[i].value);
-            if (radios[i].value == correctAnswers[0]) {
+            if (radios[i].value == correctAnswers[0]) { /*Checks whether the answer is correct*/
                 document.getElementById("displayAnswer1").innerHTML = correctAnswer + correctChoice[0];
-                marks += 2;
+                marks += 2; /*Gives 2 marks if the answer is correct*/
             } else {
                 document.getElementById("displayAnswer1").innerHTML = incorrectAnswer + correctChoice[0];
-                marks -= 1;
+                marks -= 1; /*Deducts 1 mark if the answer is correct*/
             }
             break;
         } else {
             document.getElementById("displayAnswer1").innerHTML = didntChoose + correctChoice[0];
-            marks += 0;
+            marks += 0; /*Possibly ran out of time, so nothing will be deducted from the total marks*/
         }
     }
 
@@ -235,7 +236,7 @@ function checkAnswers() {
         }
     }
 
-    if (marks >= 15) {
+    if (marks >= 15) { /*Function to change the color of the body according to the marks that the user had scored*/
         document.body.style.backgroundColor = "lawngreen";
     } else if (marks >= 10) {
         document.body.style.backgroundColor = "lightseagreen";
@@ -248,7 +249,7 @@ function checkAnswers() {
     }
 
     var m = marks.toString();
-    document.getElementById("displayMarks").innerHTML = "Your Score is: " + m;
+    document.getElementById("displayMarks").innerHTML = "Your Score is: " + m; /*Displays the total marks that the user had scored*/
 
 }
 
